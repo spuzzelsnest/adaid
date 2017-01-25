4202a477ab323ee7f8be12d1dbc8e9b06094d8d8
Juanzel <jan.mpdesmet@gmail.com>
2017-01-25 23:01:31 +0100

    Delete qdrivers.ps1

--- scripts/qdrivers.ps1
+++
@@ -1,9 +0,0 @@
-﻿#load q drive
-
-$path = C:\Users\tsy_jdesmet\desktop\Qdrives.txt
-
-Get-Content $path| foreach $user in $list 
-
-Write-Output $user
-
-#Get-ADUser -filter { DisplayName -eq "Jacobs Alain" } -properties * |select  homedirectory
