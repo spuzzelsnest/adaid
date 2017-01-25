da237b46722db696bb1459e665a1161e591b2b2e
Juanzel <jan.mpdesmet@gmail.com>
2017-01-25 22:58:13 +0100

    Update H45H1T.ps1

--- scripts/H45H/H45H1T.ps1
+++ scripts/H45H/H45H1T.ps1
@@ -9,13 +9,11 @@ $pwd = Read-Host "Password?" -assecurestring
 #[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Python27\;C:\Python27\Scripts\", "User")
 #$env:path="$env:Path;C:\Python27\;C:\Python27\Scripts"
 
-#read requirements.txt
+#read requirements.txt located in deps folder
 pip.exe install -U -r deps\requirements.txt
-
 
 Write-Output "###################################################################"
 Write-Output "#                           H45H1n T1m3                           #"
 Write-Output "###################################################################"
-
 
 python.exe .\deps\passIt.py -sam $floc\sam.save -security $floc\security.save -system ..\Examples\system.save -hashes LMHASH:NTHASH -history -k -outputfile dumps/restult local
