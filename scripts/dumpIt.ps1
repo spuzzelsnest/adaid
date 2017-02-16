50b6ee826249c10cae2897642530bb18a06234ca
Juanzel <jan.mpdesmet@gmail.com>
2017-02-16 13:20:50 +0100

    Update dumpIt.ps1

--- scripts/dumpIt.ps1
+++ scripts/dumpIt.ps1
@@ -3,7 +3,7 @@
 # NAME:		DumpIt.ps1
 #
 # AUTHOR:	Jan De Smet
-# EMAIL:	jan.de-smet@t-systems.com
+# EMAIL:	jan.mpdesmet@gmail.com
 #
 # COMMENT: 
 #			Dump files in the temp and retrive and move data to local.
